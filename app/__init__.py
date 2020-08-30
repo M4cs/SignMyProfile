d01b0e4a6b739a829b59e87073b873ca697af775
M4cs <mabridgland@protonmail.com>
2020-08-30 14:10:23 -0400

    Fix Badges

--- app/__init__.py
+++ app/__init__.py
@@ -62,7 +62,8 @@ def card(target):
     if u:
         if u.signature_count == 1:
             api = api.replace('%20Person%20Has', '%20People%20Have')
-        return redirect(api.format(amt=u.signature_count), 302)
+        res = requests.get(api.format(amt=u.signature_count)).content
+        return res, 200
     else:
         return 404
 
